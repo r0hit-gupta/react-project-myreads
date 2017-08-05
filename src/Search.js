094b8b903d0c6608f0a9e50c6a010f7141103132
r0hit-gupta <rohit1997gupta@gmail.com>
2017-08-06 01:13:46 +0530

    feat: Exception handling for book search

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,61 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+import Book from './Book';
+import Spinner from './Spinner';
+
 import * as BooksAPI from './BooksAPI';
 import './App.css';
 
 class Search extends React.Component {
   state = {
-    books: []
+    books: [],
+    resultFound: true,
+    loading: false
+  };
+
+  updateBook = (id, shelf) => {
+    BooksAPI.update(id, shelf).then(data => {
+      console.log(data);
+    });
+  };
+
+  searchBook = event => {
+    const query = event.target.value.trim();
+    if (query) {
+      this.setState({
+        loading: true,
+        resultFound: true,
+        books: []
+      });
+      BooksAPI.search(query, 20).then(books => {
+        console.log(books);
+        if (books.error) {
+          this.setState({
+            resultFound: false,
+            books: [],
+            loading: false
+          });
+        } else {
+          books = books.map(book =>
+            <Book
+              key={book.id}
+              id={book.id}
+              title={book.title}
+              author={book.authors}
+              cover={book.imageLinks.smallThumbnail}
+              shelf={book.shelf}
+              updateBook={this.updateBook}
+            />
+          );
+          this.setState({
+            resultFound: true,
+            loading: false,
+            books
+          });
+        }
+      });
+    }
   };
 
   render() {
@@ -16,11 +66,22 @@ class Search extends React.Component {
             Close
           </Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={this.searchBook}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid" />
+          <ol className="books-grid">
+            {this.state.loading && <Spinner />}
+            {!this.state.resultFound &&
+              <span>
+                No result found. Please try again with some other query.
+              </span>}
+            {this.state.books}
+          </ol>
         </div>
       </div>
     );
